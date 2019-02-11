3ef18ab79ebeed04fa955380c21bc1c850f236c2
Gordlonious <gordlonious@gmail.com>
2019-02-10 18:45:31 -0700

    added answers to project two

---
+++ 2.6_ProjectTwo.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+A = Counter(['a', 'a', 'a', 'b', 'b', 'c'])
+
+B = Counter(['a', 'a', 'b', 'b', 'b', 'd', 'd', 'd', 'd'])
+
+# A U B
+aub = A | B
+
+print('A is:        ', sorted(A.items()))
+print('B is:        ', sorted(B.items()))
+print('A union B is:', sorted(aub.items()))
+print()
+
+# A intersection B
+aib = A & B
+
+print('A is:               ', sorted(A.items()))
+print('B is:               ', sorted(B.items()))
+print('A intersection B is:', sorted(aib.items()))
+print()
+
+# A - B
+adb = A - B
+
+print('A is:    ', sorted(A.items()))
+print('B is:    ', sorted(B.items()))
+print('A - B is:', sorted(adb.items()))
+print()
+
+# A + B
+asb = A + B
+
+print('A is:    ', sorted(A.items()))
+print('B is:    ', sorted(B.items()))
+print('A + B is:', sorted(asb.items()))
