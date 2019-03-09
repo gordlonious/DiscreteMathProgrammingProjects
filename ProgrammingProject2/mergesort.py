20c64ae313dfd62bf57c39b47e6722a9c2dd4233
Gordlonious <gordlonious@gmail.com>
2019-03-08 22:29:57 -0700

    programmed comparison count aggregation into mergesort algorithm

--- ProgrammingProject2/mergesort.py
+++ ProgrammingProject2/mergesort.py
@@ -1,36 +1,72 @@
-def mergeSort(alist):
-    print("Splitting ",alist)
-    if len(alist)>1:
-        mid = len(alist)//2
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
+def mergeSortCompare(l):
 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
+    comparisonCount = [0]
 
-        i=0
-        j=0
-        k=0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
+    def mergeSort(alist, cc):
+
+        if len(alist)>1:
+
+            mid = len(alist)//2
+
+            lefthalf = alist[:mid]
+
+            righthalf = alist[mid:]
+
+            mergeSort(lefthalf, cc)
+
+            mergeSort(righthalf, cc)
+
+            (i,j,k) = (0,0,0)
+
+            while i < len(lefthalf) and j < len(righthalf):
+
+                # There could be two comparisons here
+                cc[0] += 2
+
+                if lefthalf[i] < righthalf[j]:
+
+                    cc[0] += 1
+
+                    alist[k]=lefthalf[i]
+
+                    i=i+1
+
+                else:
+
+                    alist[k]=righthalf[j]
+
+                    j=j+1
+
+                k=k+1
+
+            while i < len(lefthalf):
+
+                cc[0] += 1
+
                 alist[k]=lefthalf[i]
+
                 i=i+1
-            else:
+
+                k=k+1
+
+            while j < len(righthalf):
+
+                cc[0] += 1
+
                 alist[k]=righthalf[j]
+
                 j=j+1
-            k=k+1
 
-        while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+                k=k+1
 
-        while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
-    print("Merging ",alist)
+    print("Executing MERGESORT and counting comparison operations...")            
+
+    mergeSort(l, comparisonCount) # sort the list
+
+    return comparisonCount[0]
 
 alist = [54,26,93,17,77,31,44,55,20]
-mergeSort(alist)
-print(alist)
+
+comparisonCount = mergeSortCompare(alist)
+
+print("The number of items compared by mergesort was: {0}".format(comparisonCount))
