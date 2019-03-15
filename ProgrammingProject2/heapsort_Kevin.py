1ae0830ed911795be47b8360942535e0aedffa9a
Gordlonious <gordlonious@gmail.com>
2019-03-14 20:30:40 -0600

    added kevins heapsort to runcomparisons.py

---
+++ ProgrammingProject2/heapsort_Kevin.py
@@ -0,0 +1,84 @@
+import math
+# Python function to print permutations of a given list
+def permutation(lst): 
+  
+    # If lst is empty then there are no permutations 
+    if len(lst) == 0: 
+        return [] 
+  
+    # If there is only one element in lst then, only 
+    # one permuatation is possible 
+    if len(lst) == 1: 
+        return [lst] 
+  
+    # Find the permutations for lst if there are 
+    # more than 1 characters 
+  
+    l = [] # empty list that will store current permutation 
+  
+    # Iterate the input(lst) and calculate the permutation 
+    for i in range(len(lst)): 
+       m = lst[i] 
+  
+       # Extract lst[i] or m from the list.  remLst is 
+       # remaining list 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       # Generating all permutations where m is first 
+       # element 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
+  
+  # Driver program to test above function 
+data1 = list('0123') 
+data2 = list('012345') 
+data3 = list('01234567') 
+
+#HeapSort Algorithm
+def heapify(arr, n, i):
+    count = 0
+    largest = i  
+    l = 2 * i + 1     
+    r = 2 * i + 2     
+    if l < n and arr[i] < arr[l]:
+        largest = l
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    if largest != i:
+        count += 1
+        arr[i],arr[largest] = arr[largest],arr[i]
+        count += heapify(arr, n, largest)
+    return count
+ 
+def heapSort(arr):
+    n = len(arr)
+    count = 0
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)  
+        count += heapify(arr, i, 0)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i] 
+        count += heapify(arr, i, 0)
+ 
+    return count
+
+
+if __name__ == '__main__':
+
+        comparisons = []
+        totalcomparisons = 0
+        for p in permutation(data3):
+            arr = p
+        
+            comparisons.append(heapSort(arr))
+#print(comparisons)
+        comparisons.sort(reverse = True)
+        print('Worst 10: ')
+        print(comparisons[:10])
+        comparisons.sort()
+        print('Best 10: ')
+        print(comparisons[:10])
+        totalcomparisons = sum(comparisons)
+        print('Average = ' + str(totalcomparisons/math.factorial(8)))
+
--- ProgrammingProject2/runcomparisons.py
+++ ProgrammingProject2/runcomparisons.py
@@ -4,29 +4,31 @@
 #Zhengyu Wu
 #Brad Rohbock 
 
+import heapsort_Kevin
 from itertools import permutations
 from mergesort import mergeSortCompare
 
 # PARAM sort: must give this function a sort function that returns a comparison count
-# RETURNS: a list of pairs where (x,y) = (n,comparisonCount)
+# RETURNS: a list of pairs where (x,y,z) = (n,comparisonCount,permutation)
 #### the list will return one n!/(n-r)! pairs for n=4, n=6, and n=8, giving you a length of 4! + 6! + 8!
 def RunComparisons(sort):
     
-    ncountpairs = []
+    ncomparperm = []
     
     for p in permutations(range(4)):
-        ncountpairs.append([4, sort(list(p)), p])
+        ncomparperm.append([4, sort(list(p)), p])
 
     for p in permutations(range(6)):
-        ncountpairs.append([6, sort(list(p)), p])
+        ncomparperm.append([6, sort(list(p)), p])
 
     for p in permutations(range(8)):
-        ncountpairs.append([8, sort(list(p)), p])
+        ncomparperm.append([8, sort(list(p)), p])
 
-    return ncountpairs
+    return ncomparperm
 
 # PARAM: data | the comparison data in the form [(n, comparisonCount)]
 # PARAM: n | the class of comparison you would like the wors case of (i.e 4, 6, 8)
+# PARAM: data | a list in the form [n, comparison count for a permutation ran through the sort, the permutation]
 def GetWorstCaseComparison(n, data):
     
     nonly = list(filter(lambda x: x[0] == n, data))
@@ -38,8 +40,36 @@ def GetWorstCaseComparison(n, data):
 
     return max(justcomparisons)
 
-# main script..
+# PARAM: data | the comparison data in the form [(n, comparisonCount)]
+# PARAM: n | the class of comparison you would like the wors case of (i.e 4, 6, 8)
+# PARAM: data | a list in the form [n, comparison count for a permutation ran through the sort, the permutation]
+def GetTenBest(n, data):
+
+    nonly = list(filter(lambda x: x[0] == n, data))
+
+    justcomparisons = []
+
+    for x in nonly:
+        justcomparisons.append(x[1])
+
+    tenbest = []
+
+    for i in range(10):
+        tenbest.append(justcomparisons[i])
+
+    return tenbest
+    
+
+# the below code should use all sorting algorithms and data to answer part 4 
 
 mergeSortData = RunComparisons(mergeSortCompare)
+heapSortData = RunComparisons(heapsort_Kevin.heapSort)
 
+print('mergesort worst case for n=4:')
 print(GetWorstCaseComparison(4, mergeSortData))
+
+#print('mergesort ten best cases for n=8')
+#print(GetTenBest(8, mergeSortData))
+
+print('heapsort worst case for n=8')
+print(GetWorstCaseComparison(8, heapSortData))
