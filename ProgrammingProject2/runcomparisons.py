d80eb53e4b5e38e10ef88ab6fcaedf3e2acce073
Gordlonious <gordlonious@gmail.com>
2019-03-17 16:34:53 -0600

    printed additional n class data for best/worst case (from kevin)

--- ProgrammingProject2/runcomparisons.py
+++ ProgrammingProject2/runcomparisons.py
@@ -1,8 +1,8 @@
-#Discrete Mathematics
-#CSIS Programming Project 2
-#Gordon Portzline
-#Zhengyu Wu
-#Brad Rohbock 
+# Discrete Mathematics
+# CSIS Programming Project 2
+# Gordon Portzline
+# Zhengyu Wu
+# Brad Rohbock
 
 import quicksort
 import heapsort_Kevin
@@ -14,31 +14,57 @@ import math
 from Part4Data import Part4Data
 from CallPrintMethods import CallAllPrintMethods
 
+
 # PARAM sort: must give this function a sort function that returns a comparison count
-# RETURNS: a ComparisonData object 
+# RETURNS: a ComparisonData object
 #### the Data member should have n!/(n-r)! items for n=4, n=6, and n=8, giving you a length of 4! + 6! + 8!
-def RunComparisons(sort):
-    
+def RunComparisons4(sort):
     ncomparperm = ComparisonData()
-    
+
     for p in permutations(range(4)):
         ncomparperm.AddData(4, sort(list(p)), p)
 
+
+    return ncomparperm
+
+def RunComparisons6(sort):
+    ncomparperm = ComparisonData()
+
     for p in permutations(range(6)):
         ncomparperm.AddData(6, sort(list(p)), p)
+
+    return ncomparperm
+
+def RunComparisons8(sort):
+    ncomparperm = ComparisonData()
 
     for p in permutations(range(8)):
         ncomparperm.AddData(8, sort(list(p)), p)
 
     return ncomparperm
+# the below code should use all sorting algorithms and data to answer part 4
 
 
-# the below code should use all sorting algorithms and data to answer part 4 
+mergeSortData4 = Part4Data(RunComparisons4(mergeSortCompare), "mergesort")
+heapSortData4 = Part4Data(RunComparisons4(heapsort_Kevin.heapSort), "heapsort")
+quickSortData4 = Part4Data(RunComparisons4(quicksort.quicksort), "quicksort")
 
-mergeSortData = Part4Data(RunComparisons(mergeSortCompare), "mergesort")
-heapSortData = Part4Data(RunComparisons(heapsort_Kevin.heapSort), "heapsort")
-quickSortData = Part4Data(RunComparisons(quicksort.quicksort), "quicksort")
+CallAllPrintMethods(mergeSortData4)
+CallAllPrintMethods(heapSortData4)
+CallAllPrintMethods(quickSortData4)
 
-CallAllPrintMethods(mergeSortData)
-CallAllPrintMethods(heapSortData)
-CallAllPrintMethods(quickSortData)
+mergeSortData6 = Part4Data(RunComparisons6(mergeSortCompare), "mergesort")
+heapSortData6 = Part4Data(RunComparisons6(heapsort_Kevin.heapSort), "heapsort")
+quickSortData6 = Part4Data(RunComparisons6(quicksort.quicksort), "quicksort")
+
+CallAllPrintMethods(mergeSortData6)
+CallAllPrintMethods(heapSortData6)
+CallAllPrintMethods(quickSortData6)
+
+mergeSortData8 = Part4Data(RunComparisons8(mergeSortCompare), "mergesort")
+heapSortData8 = Part4Data(RunComparisons8(heapsort_Kevin.heapSort), "heapsort")
+quickSortData8 = Part4Data(RunComparisons8(quicksort.quicksort), "quicksort")
+
+CallAllPrintMethods(mergeSortData8)
+CallAllPrintMethods(heapSortData8)
+CallAllPrintMethods(quickSortData8)
