6be2f13e17206e37f9cc14636eb72664276f67c2
Gordlonious <gordlonious@gmail.com>
2019-03-30 15:50:54 -0600

    overrode equals and hashCode for Experiment type

--- ProgrammingProject3/basicexperiments/Experiment.java
+++ ProgrammingProject3/basicexperiments/Experiment.java
@@ -14,4 +14,28 @@ public class Experiment {
 	public int Weight;
 	
 	public int Rating;
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this) return true;
+		
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		
+		Experiment expmnt = (Experiment) obj;
+		
+		return this.Weight == expmnt.Weight && this.Name.equals(expmnt.Name) && this.Rating == expmnt.Rating;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		int result = 17;
+		
+		result = 31 * result + this.Weight;
+		result = 31 * result + this.Rating;
+		result = 31 * result + this.Name.hashCode();
+		
+		return result;
+	}
 }
--- ProgrammingProject3/basicexperiments/Experiments.java
+++ ProgrammingProject3/basicexperiments/Experiments.java
@@ -1,12 +1,26 @@
 package basicexperiments;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Experiments {
 	
 	private Experiments()
 	{
-		
+		ExperimentsList = Arrays.asList(
+			new Experiment("Cloud Patterns", 36, 5),
+			new Experiment("Solar Flares", 264, 9),
+			new Experiment("Solar Power", 188, 6),
+			new Experiment("Binary Stars", 203, 8),
+			new Experiment("Relativity", 104, 8),
+			new Experiment("Seed Viability", 7, 4),
+			new Experiment("Sun Spots", 90, 2),
+			new Experiment("Mice Tumors", 65, 8),
+			new Experiment("Microgravity Plant Growth", 75, 5),
+			new Experiment("Micrometeorites", 170, 9),
+			new Experiment("Cosmic Rays", 80, 7),
+			new Experiment("Yeast Fermentation", 27, 4)
+		);
 	}
 	
-	public static ArrayList<Experiment> ExperimentsList;
+	public static List<Experiment> ExperimentsList;
 }
