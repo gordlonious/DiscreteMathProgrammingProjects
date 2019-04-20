ef6ffab4d9610b1fd0e21f77a26ec1c28e98074f
Gordlonious <gordlonious@gmail.com>
2019-04-20 00:20:27 -0600

    full code for part 1, possible bug though: sometimes generates bigger number for q1 than for q2

--- ProgrammingProject4/part1.fs
+++ ProgrammingProject4/part1.fs
@@ -8,19 +8,21 @@ let chanceBool chance =
 
     Seq.contains (r.Next 100) {0 .. chance}
 
-let getReliableCount min max =
+let getReliableCount (min:double, max:double):int =
 
-    let rec get inTargetCount offTargetCount callsToChanceBoolCount min max =
-        
-        //printfn "%d %d" inTargetCount offTargetCount
-        if offTargetCount <> 0 && (inTargetCount / offTargetCount) >= min && (inTargetCount / offTargetCount) <= max
+    let rec get (inTargetCount:double, offTargetCount:double, callsToChanceBoolCount:int):int =
+
+        let percentTrue = if callsToChanceBoolCount <> 0 then (inTargetCount / (double callsToChanceBoolCount)*100.) else 0.
+
+        if percentTrue >= min && percentTrue <= max
         then callsToChanceBoolCount
         else
             match chanceBool Target with
-            | true -> get (inTargetCount+1) offTargetCount (callsToChanceBoolCount+1) min max
-            | false -> get inTargetCount (offTargetCount+1) (callsToChanceBoolCount+1) min max
+            | true -> get ((inTargetCount+1.), offTargetCount, (callsToChanceBoolCount+1))
+            | false -> get (inTargetCount, (offTargetCount+1.), (callsToChanceBoolCount+1))
 
-    get 0 0 0 min max
+    get (0., 0., 0)
 
-printfn "%d" (getReliableCount 60 80)
-//printfn "%d" (getReliableCount 69.9 71.1)
+printfn "%d" (getReliableCount(69., 71.))
+
+printfn "%d" (getReliableCount(69.9, 71.1))
